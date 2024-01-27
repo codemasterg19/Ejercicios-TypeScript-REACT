3cc2a8b5bb89b08422b32343368fcc3ac178d7c8
codemasterg19 <pablo_jmz1@hotmail.com>
2024-01-27 11:15:59 -0500

    Ejercicios React

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+
+export const App = () => {
+  return (
+    <div>
+      <h1>Ejercicios TypeScript - React</h1>
+      
+    </div>
+  )
+}
+
+//exportando el componente a todo el proyecto
+export default App;
